91f956cf447b2ddde07b3575f920456c0199f57d
Nicolas <nicojs2011@gmail.com>
2026-04-28 07:46:07 -0400

    agregado mapa de venezuela y corregido color de pie de pagina

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-secondary text-center p-2 text-white">
+        <footer className="bg-inm-secondary text-center p-2 text-white">
             <p>© Copyright {new Date().getFullYear()} Inamujer. Oficina de Sistema y Tecnología de la Información</p>
         </footer>
     )
