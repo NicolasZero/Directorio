cfb3e19baf276e513527d8e7bf3bba67ee0b8512
Nicolas <nicojs2011@gmail.com>
2026-05-06 20:24:29 -0400

    solucion para que buildee en render.com

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router';
-import AppRouter from './router/appRouter.tsx'
+import AppRouter from './router/AppRouter'
 import { ThemeProvider } from "./context/theme-provider";
 
 createRoot(document.getElementById('root')!).render(
