0c99581ceb495c84cd8806414145c7ae78d3240d
Nicolas Zapata <nicojs2011@gmail.com>
2026-06-11 12:17:29 -0400

    rutas publicas y privadas separadas

--- apps/api/src/routes/directory.js
+++ apps/api/src/routes/directory.js
@@ -2,10 +2,12 @@ import { authenticate } from '../middlewares/authMiddleware.js'
 import { getDirectories, getDirectoryById, createDirectory, updateDirectory, deleteDirectory } from '../controllers/directoryController.js'
 
 export default async function (fastify) {
-    fastify.addHook('preHandler', authenticate)
+    // GET routes are public
     fastify.get('/', getDirectories)
     fastify.get('/:id', getDirectoryById)
-    fastify.post('/', createDirectory)
-    fastify.put('/:id', updateDirectory)
-    fastify.delete('/:id', deleteDirectory)
+
+    // Protected routes: require authentication
+    fastify.post('/', { preHandler: authenticate }, createDirectory)
+    fastify.put('/:id', { preHandler: authenticate }, updateDirectory)
+    fastify.delete('/:id', { preHandler: authenticate }, deleteDirectory)
 }
--- apps/api/src/routes/location.js
+++ apps/api/src/routes/location.js
@@ -1,8 +1,8 @@
-import { authenticate } from "../middlewares/authMiddleware.js"
+// import { authenticate } from "../middlewares/authMiddleware.js"
 import { statesAll, stateById, municipalitiesAll, municipalityByStateId, allLocations } from "../controllers/locationController.js"
 
 export default async function (fastify) {
-    fastify.addHook("preHandler", authenticate)
+    // fastify.addHook("preHandler", authenticate)
     fastify.get("/", allLocations)
     fastify.get("/state", statesAll)
     fastify.get("/state/:id", stateById)
