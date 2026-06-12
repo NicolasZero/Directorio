191cc4d0b26e57c63ded863a2d67af183d583e7d
Nicolas <nicojs2011@gmail.com>
2026-06-12 18:32:39 -0400

    fix: ajustes a las rutas publicas privadas

--- apps/api/src/routes/class.js
+++ apps/api/src/routes/class.js
@@ -2,10 +2,12 @@ import { authenticate } from '../middlewares/authMiddleware.js'
 import { createClass, getClasses, getClassById, updateClass, deleteClass } from '../controllers/classController.js'
 
 export default async function (fastify) {
-  fastify.addHook('preHandler', authenticate)
+  // GET routes are public
   fastify.get('/', getClasses)
   fastify.get('/:id', getClassById)
-  fastify.post('/', createClass)
-  fastify.put('/:id', updateClass)
-  fastify.delete('/:id', deleteClass)
+
+  // Protected routes: require authentication
+  fastify.post('/', { preHandler: authenticate }, createClass)
+  fastify.put('/:id', { preHandler: authenticate }, updateClass)
+  fastify.delete('/:id', { preHandler: authenticate }, deleteClass)
 }
